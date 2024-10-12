b88a2e88df59a4edd4d381b8ba86e0ad1a961e37
aka <akadev8@outlook.com>
2024-10-12 18:04:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 
+### Step 3: Populate `setup.py`
+
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name="SEOAscend",
+    version="0.1.0",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        # Add your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'seoascend=src.main:main',
+        ],
+    },
+)
+
